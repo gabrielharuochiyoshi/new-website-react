15762933696cad203e72e5b09658fa4b8e36e8ae
gabrielharuochiyoshi <gabriel.haruo2016@gmail.com>
2022-06-19 00:06:16 -0300

    Footer Finished

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { Button } from "../../globalStyles";
 import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaLinkedin,
+} from "react-icons/fa";
+import {
   FooterContainer,
   FooterSubscription,
   FooterSubHeading,
@@ -12,6 +18,13 @@ import {
   FooterLinkTitle,
   FooterLinksItems,
   FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink,
+  SocialIcon,
 } from "./Footer.elements";
 
 const Footer = () => {
@@ -41,31 +54,69 @@ const Footer = () => {
             <FooterLink to="/">Terms of Service</FooterLink>
           </FooterLinksItems>
           <FooterLinksItems>
-            <FooterLinkTitle>Contato</FooterLinkTitle>
-            <FooterLink to="/sign-up">How it works</FooterLink>
-            <FooterLink to="/">Testimonials</FooterLink>
-            <FooterLink to="/">Carrers</FooterLink>
-            <FooterLink to="/">Investors</FooterLink>
-            <FooterLink to="/">Terms of Service</FooterLink>
+            <FooterLinkTitle>Contact</FooterLinkTitle>
+            <FooterLink to="/">Contact</FooterLink>
+            <FooterLink to="/">Support</FooterLink>
+            <FooterLink to="/">Destinations</FooterLink>
+            <FooterLink to="/">Sponsorships</FooterLink>
           </FooterLinksItems>
+        </FooterLinksWrapper>
+        <FooterLinksWrapper>
           <FooterLinksItems>
             <FooterLinkTitle>Videos</FooterLinkTitle>
-            <FooterLink to="/sign-up">How it works</FooterLink>
-            <FooterLink to="/">Testimonials</FooterLink>
-            <FooterLink to="/">Carrers</FooterLink>
-            <FooterLink to="/">Investors</FooterLink>
-            <FooterLink to="/">Terms of Service</FooterLink>
+            <FooterLink to="/">Submit Video</FooterLink>
+            <FooterLink to="/">Ambassadors</FooterLink>
+            <FooterLink to="/">Agency</FooterLink>
+            <FooterLink to="/">Influencer</FooterLink>
           </FooterLinksItems>
           <FooterLinksItems>
             <FooterLinkTitle>Social Media</FooterLinkTitle>
-            <FooterLink to="/sign-up">How it works</FooterLink>
-            <FooterLink to="/">Testimonials</FooterLink>
-            <FooterLink to="/">Carrers</FooterLink>
-            <FooterLink to="/">Investors</FooterLink>
-            <FooterLink to="/">Terms of Service</FooterLink>
+            <FooterLink to="/">Facebook</FooterLink>
+            <FooterLink to="/">Instagram</FooterLink>
+            <FooterLink to="/">Linkedin</FooterLink>
+            <FooterLink href="https://github.com/gabrielharuochiyoshi">Github</FooterLink>
           </FooterLinksItems>
         </FooterLinksWrapper>
       </FooterLinksContainer>
+      <SocialMedia>
+        <SocialMediaWrap>
+          <SocialLogo to="/">
+            <SocialIcon />
+            ULTRA
+          </SocialLogo>
+          <WebsiteRights>ULTRA © 2022</WebsiteRights>
+          <SocialIcons>
+            <SocialIconLink
+              href={"https://www.facebook.com/gabriel.haruo.96/"}
+              target="_blank"
+              arial-label="Facebook"
+            >
+              <FaFacebook />
+            </SocialIconLink>
+            <SocialIconLink
+              href={"https://www.instagram.com/haruochiyoshi/"}
+              target="_blank"
+              arial-label="Instagram"
+            >
+              <FaInstagram />
+            </SocialIconLink>
+            <SocialIconLink
+              href={"https://www.youtube.com/"}
+              target="_blank"
+              arial-label="Youtube"
+            >
+              <FaYoutube />
+            </SocialIconLink>
+            <SocialIconLink
+              href={"https://www.linkedin.com/in/gabriel-haruo-chiyoshi/"}
+              target="_blank"
+              arial-label="Linkedin"
+            >
+              <FaLinkedin />
+            </SocialIconLink>
+          </SocialIcons>
+        </SocialMediaWrap>
+      </SocialMedia>
     </FooterContainer>
   );
 };
